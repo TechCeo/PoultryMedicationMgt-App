0af10ba6ea0f98d43117e3a16871ec16f757f053
TechCeo <adamuyusuf3g@gmail.com>
2022-03-24 10:23:22 +0100

    3 commit

---
+++ PoultryMedicMgtApp.py
@@ -0,0 +1,444 @@
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+#from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtPrintSupport import *
+import datetime
+from datetime import date
+from datetime import datetime, timedelta
+from win10toast import ToastNotifier
+import sys
+import sqlite3
+import time
+import os
+
+
+#label.setFont.setStyleSheet("font-weight: bold")
+#myFont=QtGui.QFont()
+#myFont.setBold(True)
+#self.label.setFont(myFont)
+
+
+class WorkerThread(QThread):
+    def run(self):
+        
+        try:
+            self.conn = sqlite3.connect("database.db")
+            self.c = self.conn.cursor()
+            result = self.c.execute("SELECT mobile from students")
+            row = result.fetchall()
+            result2 = self.c.execute("SELECT Name from students")
+            row2 = result2.fetchall()
+            #print(row)
+            #serachresult = "Exp_Date : "+str(row[4])
+            #QMessageBox.information(QMessageBox(), 'Successful', serachresult)
+            
+            
+        
+            #except Exception:
+            #   QMessageBox.warning(QMessageBox(), 'Error', 'Could not Find Product from the database.')
+            #print(serachresult)
+
+
+            today = date.today()
+            exp_window = (today + timedelta(0)).strftime("%d-%m-%Y")
+            # dd/mm/YY
+            #d1 = today.strftime("%d-%m-%Y")
+            #print("d1 =", d1)
+            namelist=[]
+            for i in row2:
+                row2real="".join(i)
+                namelist.append(row2real)
+                
+            numeral = []
+            
+        
+            for i in row:
+                numeral.append("a")
+                index=numeral.count("a")
+                
+                y = ''.join(i)
+                x = datetime.strptime(y, '%d-%m-%Y')
+                #print (y + "and " + exp_window)
+                if y==exp_window:
+                    #print("False " + y + "is greater than" + exp_window )
+                    #pass;
+                #elif int(y)==int(exp_window):
+                    hr=ToastNotifier()
+                    hr.show_toast('alarm',"Administer This Medication Today: " + namelist[index] , icon_path = "icon/notification-icon-bell-alarm2.ico", duration=5 )
+                    #print("True " + y + "is less than" + self.exp_window )print
+                    
+                    
+                
+                #elif int(y[0:2])>=int(exp_window[0:2]):
+                    #print("False " + y + "is greater than" + exp_window )
+                    #pass;
+                #else:
+                    #print("True " + y + "is less than" + exp_window )
+                    #numeral.append("a")
+                    #index=numeral.count("a")
+                    #self.hr=ToastNotifier()
+                    #self.hr.show_toast('alarm',"Administer This Medication Today: " + namelist[index] , icon_path = "icon/notification-icon-bell-alarm2.ico", duration=5)
+            
+            self.conn.commit()
+            self.c.close()
+            self.conn.close()
+
+        except:
+            pass
+            #Print("Issues with Notification")
+            
+        
+        #conn.commit()
+        #c.close()
+        #conn.close()
+
+
+
+
+
+
+
+
+class InsertDialog(QDialog):
+    def __init__(self, *args, **kwargs):
+        super(InsertDialog, self).__init__(*args, **kwargs)
+
+        self.QBtn = QPushButton()
+        self.QBtn.setText("Add")
+
+        self.setWindowTitle("Add Medication to Schedule")
+        self.setFixedWidth(300)
+        self.setFixedHeight(300)
+
+        self.setWindowTitle("Add Medication to Schedule")
+        self.setFixedWidth(300)
+        self.setFixedHeight(300)
+
+        self.QBtn.clicked.connect(self.addstudent)
+
+        layout = QVBoxLayout()
+
+        self.nameinput = QLineEdit()
+        self.nameinput.setPlaceholderText("Medication Name")
+        layout.addWidget(self.nameinput)
+
+        self.branchinput = QComboBox()
+        self.branchinput.addItem("Injection")
+        self.branchinput.addItem("Syrup")
+        self.branchinput.addItem("Water-based")
+        self.branchinput.addItem("Air-borne")
+        self.branchinput.addItem("Spray")
+        self.branchinput.addItem("Antiseptic")
+        self.branchinput.addItem("Feed-based")
+        layout.addWidget(self.branchinput)
+
+        self.seminput = QComboBox()
+        self.seminput.addItem("Oral")
+        self.seminput.addItem("IM")
+        self.seminput.addItem("Topical")
+        self.seminput.addItem("Intratracheal")
+        self.seminput.addItem("Oral(feed)")
+        self.seminput.addItem("Oral(water)")
+        self.seminput.addItem("SC")
+        self.seminput.addItem("IV")
+        layout.addWidget(self.seminput)
+
+        self.mobileinput = QLineEdit()
+        self.mobileinput.setPlaceholderText("Medication Date(dd-mm-yy)")
+        layout.addWidget(self.mobileinput)
+
+        self.addressinput = QLineEdit() ####where you insert Remark
+        self.addressinput.setPlaceholderText("Dosage")
+        layout.addWidget(self.addressinput)
+
+        layout.addWidget(self.QBtn)
+        self.setLayout(layout)
+
+    def addstudent(self):
+
+        name = ""
+        branch = ""
+        sem = -1
+        mobile = ""
+        address = ""
+
+        name = self.nameinput.text()
+        branch = self.branchinput.itemText(self.branchinput.currentIndex())
+        sem = self.seminput.itemText(self.seminput.currentIndex())
+        mobile = self.mobileinput.text()
+        address = self.addressinput.text()
+        try:
+            self.conn = sqlite3.connect("database.db")
+            self.c = self.conn.cursor()
+            self.c.execute("INSERT INTO students (name,branch,sem,Mobile,address) VALUES (?,?,?,?,?)",(name,branch,sem,mobile,address))
+            self.conn.commit()
+            self.c.close()
+            self.conn.close()
+            QMessageBox.information(QMessageBox(),'Successful','Medication successfully added to DB.')
+            self.close()
+        except Exception:
+            QMessageBox.warning(QMessageBox(), 'Error', 'Could not add Medication DB.')
+
+class SearchDialog(QDialog):
+    def __init__(self, *args, **kwargs):
+        super(SearchDialog, self).__init__(*args, **kwargs)
+
+        self.QBtn = QPushButton()
+        self.QBtn.setText("Search")
+
+        self.setWindowTitle("Search Medication")
+        self.setFixedWidth(300)
+        self.setFixedHeight(100)
+        self.QBtn.clicked.connect(self.searchstudent)
+        layout = QVBoxLayout()
+
+        self.searchinput = QLineEdit()
+        self.onlyInt = QIntValidator()
+        self.searchinput.setValidator(self.onlyInt)
+        self.searchinput.setPlaceholderText("Roll No.")
+        layout.addWidget(self.searchinput)
+        layout.addWidget(self.QBtn)
+        self.setLayout(layout)
+
+    def searchstudent(self):
+
+        searchrol = ""
+        searchrol = self.searchinput.text()
+        try:
+            self.conn = sqlite3.connect("database.db")
+            self.c = self.conn.cursor()
+            result = self.c.execute("SELECT * from students WHERE roll="+str(searchrol))
+            row = result.fetchone()
+            serachresult = "Rollno : "+str(row[0])+'\n'+"Name : "+str(row[1])+'\n'+"Drug : "+str(row[2])+'\n'+"Medication Type : "+str(row[3])+'\n'+"Medication_date : "+str(row[4])
+            QMessageBox.information(QMessageBox(), 'Successful', serachresult)
+            self.conn.commit()
+            self.c.close()
+            self.conn.close()
+        except Exception:
+            QMessageBox.warning(QMessageBox(), 'Error', 'Could not Find this Medication in the database.')
+
+class DeleteDialog(QDialog):
+    def __init__(self, *args, **kwargs):
+        super(DeleteDialog, self).__init__(*args, **kwargs)
+
+        self.QBtn = QPushButton()
+        self.QBtn.setText("Delete")
+
+        self.setWindowTitle("Delete Medication")
+        self.setFixedWidth(300)
+        self.setFixedHeight(100)
+        self.QBtn.clicked.connect(self.deletestudent)
+        layout = QVBoxLayout()
+
+        self.deleteinput = QLineEdit()
+        self.onlyInt = QIntValidator()
+        self.deleteinput.setValidator(self.onlyInt)
+        self.deleteinput.setPlaceholderText("Roll No.")
+        layout.addWidget(self.deleteinput)
+        layout.addWidget(self.QBtn)
+        self.setLayout(layout)
+
+    def deletestudent(self):
+
+        delrol = ""
+        delrol = self.deleteinput.text()
+        try:
+            self.conn = sqlite3.connect("database.db")
+            self.c = self.conn.cursor()
+            self.c.execute("DELETE from students WHERE roll="+str(delrol))
+            self.conn.commit()
+            self.c.close()
+            self.conn.close()
+            QMessageBox.information(QMessageBox(),'Successful','Medication Succesfully Deleted From DB')
+            self.close()
+        except Exception:
+            QMessageBox.warning(QMessageBox(), 'Error', 'Could not Delete Medication from the DB.')
+
+
+
+
+
+
+class AboutDialog(QDialog):
+    def __init__(self, *args, **kwargs):
+        super(AboutDialog, self).__init__(*args, **kwargs)
+
+        self.setFixedWidth(500)
+        self.setFixedHeight(250)
+
+        QBtn = QDialogButtonBox.Ok  
+        self.buttonBox = QDialogButtonBox(QBtn)
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+
+        layout = QVBoxLayout()
+        
+        self.setWindowTitle("About")
+        title = QLabel("Poultry Medication Management System")
+        font = title.font()
+        font.setPointSize(20)
+        title.setFont(font)
+
+        labelpic = QLabel()
+        pixmap = QPixmap('TechCeoIcon.png')
+        pixmap = pixmap.scaledToWidth(275)
+        labelpic.setPixmap(pixmap)
+        labelpic.setFixedHeight(150)
+
+        layout.addWidget(title)
+
+        layout.addWidget(QLabel("v1.0"))
+        layout.addWidget(QLabel("Copyright Okay TECHCEO  2021"))
+        layout.addWidget(labelpic)
+
+
+        layout.addWidget(self.buttonBox)
+
+        self.setLayout(layout)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setWindowIcon(QIcon('icon/micon.ico'))  #window icon
+
+        self.conn = sqlite3.connect("database.db")
+        self.c = self.conn.cursor()
+        self.c.execute("CREATE TABLE IF NOT EXISTS students(roll INTEGER PRIMARY KEY AUTOINCREMENT ,name TEXT,branch TEXT,sem INTEGER,mobile INTEGER,address TEXT)")
+        self.c.close()
+
+        file_menu = self.menuBar().addMenu("&File")
+
+        help_menu = self.menuBar().addMenu("&About")
+        self.setWindowTitle("POULTRY MEDICATION MANAGEMENT SYSTEM")
+        self.setMinimumSize(800, 600)
+
+        self.tableWidget = QTableWidget()
+        self.setCentralWidget(self.tableWidget)
+        self.tableWidget.setAlternatingRowColors(True)
+        self.tableWidget.setColumnCount(6)
+        self.tableWidget.horizontalHeader().setCascadingSectionResizes(False)
+        self.tableWidget.horizontalHeader().setSortIndicatorShown(False)
+        self.tableWidget.horizontalHeader().setStretchLastSection(True)
+        self.tableWidget.verticalHeader().setVisible(False)
+        self.tableWidget.verticalHeader().setCascadingSectionResizes(False)
+        self.tableWidget.verticalHeader().setStretchLastSection(False)
+        self.tableWidget.setHorizontalHeaderLabels(("Roll No.","Drug","Medication Type","Route","Medication_Date","Dosage"))
+
+        toolbar = QToolBar()
+        toolbar.setMovable(False)
+        self.addToolBar(toolbar)
+
+        statusbar = QStatusBar()
+        self.setStatusBar(statusbar)
+
+        btn_ac_adduser = QAction(QIcon("icon/add-list-x.png"), "Add to Medication Schedule", self)   #add student icon
+        btn_ac_adduser.triggered.connect(self.insert)
+        btn_ac_adduser.setStatusTip("Add New Medication to Schedule")
+        toolbar.addAction(btn_ac_adduser)
+
+        btn_ac_refresh = QAction(QIcon("icon/Refresh.png"),"Refresh",self)   #refresh icon
+        btn_ac_refresh.triggered.connect(self.loaddata)
+        btn_ac_refresh.setStatusTip("Refresh")
+        toolbar.addAction(btn_ac_refresh)
+
+        btn_ac_search = QAction(QIcon("icon/search.png"), "Search Medication", self)  #search icon
+        btn_ac_search.triggered.connect(self.search)
+        btn_ac_search.setStatusTip("Search Medication")
+        toolbar.addAction(btn_ac_search)
+
+        btn_ac_delete = QAction(QIcon("icon/delete.png"), "Delete Medication", self)
+        btn_ac_delete.triggered.connect(self.delete)
+        btn_ac_delete.setStatusTip("Delete Medication")
+        toolbar.addAction(btn_ac_delete)
+
+        adduser_action = QAction(QIcon("icon/add-list-x.png"),"Add to Medication Schedule", self)
+        adduser_action.triggered.connect(self.insert)
+        file_menu.addAction(adduser_action)
+
+        searchuser_action = QAction(QIcon("icon/search.png"), "Search Medication", self)
+        searchuser_action.triggered.connect(self.search)
+        file_menu.addAction(searchuser_action)
+
+        deluser_action = QAction(QIcon("icon/delete.png"), "Delete", self)
+        deluser_action.triggered.connect(self.delete)
+        file_menu.addAction(deluser_action)
+
+
+        about_action = QAction(QIcon("icon/i1.png"),"Developer", self)  #info icon
+        about_action.triggered.connect(self.about)
+        help_menu.addAction(about_action)
+
+    def loaddata(self):
+        self.connection = sqlite3.connect("database.db")
+        query = "SELECT * FROM students"
+        result = self.connection.execute(query)
+        self.tableWidget.setRowCount(0)
+        for row_number, row_data in enumerate(result):
+            self.tableWidget.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.tableWidget.setItem(row_number, column_number,QTableWidgetItem(str(data)))
+        self.connection.close()
+        
+        
+
+                
+        
+
+        #print(y)
+
+
+        #print(exp_window)
+        #print(row)
+
+
+    def handlePaintRequest(self, printer):
+        document = QTextDocument()
+        cursor = QTextCursor(document)
+        model = self.table.model()
+        table = cursor.insertTable(
+            model.rowCount(), model.columnCount())
+        for row in range(table.rows()):
+            for column in range(table.columns()):
+                cursor.insertText(model.item(row, column).text())
+                cursor.movePosition(QTextCursor.NextCell)
+        document.print_(printer)
+
+    def insert(self):
+        dlg = InsertDialog()
+        dlg.exec_()
+
+    def delete(self):
+        dlg = DeleteDialog()
+        dlg.exec_()
+
+    def search(self):
+        dlg = SearchDialog()
+        dlg.exec_()
+
+    def about(self):
+        dlg = AboutDialog()
+        dlg.exec_()
+
+
+        
+        
+x = WorkerThread()
+x.start()
+
+#app1=Notification()
+app = QApplication(sys.argv)
+#app.setWindowIcon(QIcon('icon/download.ico'))
+if(QDialog.Accepted == True):
+    window = MainWindow()
+    #window.setWindowIcon(QtGui.QIcon('appicon.ico'))
+    window.show()
+    window.loaddata()
+    
+sys.exit(app.exec_())
+
+#app1=Notification()
+
+
+
